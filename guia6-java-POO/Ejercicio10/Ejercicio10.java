ce3a1fc4e7e214a6f1bbe8b75c7ec48767e69207
Eugenia <costamariaeugenia1@gmail.com>
2021-07-15 16:31:22 -0300

    Ejercicio 10

---
+++ guia6-java-POO/Ejercicio10/Ejercicio10.java
@@ -0,0 +1,65 @@
+package Ejercicio10;
+import java.util.Arrays;
+
+public class Ejercicio10 {
+
+
+    public static void main(String[] args) {
+        
+        double arrayA[];
+        double arrayB[];
+        
+        arrayA = new double [50];
+        arrayB = new double [20];
+        
+        //Relleno mi vector con numeros aleatorios
+        for (int i = 0; i < 50; i++) {
+            //arrayA[i]= (double) Math.round(Math.random() * 50);
+            Arrays.fill(arrayA, i, i+1, (double) Math.round(Math.random() * 50));
+        }
+        
+        System.out.println("Array A : ");
+        System.out.println(" ");
+        
+        mostrarVector(arrayA);
+        
+        // System.out.println(Arrays.toString(arrayA));
+        
+        //Para ordenarlos en forma ascendente
+        Arrays.sort(arrayA);
+        
+        System.out.println("Array A ordenado: ");
+        mostrarVector(arrayA);
+        
+        
+        System.out.println("Array B:");
+        //Desde que array tomo los numeros
+        //Desde que posicion tomo los numeros del Array original
+        //A que array pongo los numeros
+        //La posicion inicial del que le paso los numeros
+        //Cuantos numeros paso
+        System.arraycopy(arrayA, 0, arrayB, 0, 10);
+        
+        //Si tenia 0,10 el problema es que cambia la dimension del vector
+        //arrayB = Arrays.copyOfRange(arrayA, 0 ,20);  
+  
+        Arrays.fill(arrayB, 10, 20 , 0.5);
+        mostrarVector(arrayB);
+        
+        
+        
+    }
+    
+    //Para imprimir vectores
+    private static void mostrarVector(double [] array) {
+        System.out.println(" ");
+        
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array [i]);
+        }
+        
+        System.out.println(" ");
+    
+    }
+    
+}
