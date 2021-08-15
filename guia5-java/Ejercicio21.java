4e6727c868f21893d6310d9201f2afe733e72323
Eugenia <costamariaeugenia1@gmail.com>
2021-08-15 17:11:15 -0300

    Update Ejercicio21.java

--- guia5-java/Ejercicio21.java
+++ guia5-java/Ejercicio21.java
@@ -7,14 +7,14 @@ package ejercicio21;
 public class Ejercicio21 {
 
     public static void main(String[] args) {
-     //vector como entero, con dimension de 100
-       int n=100;
+     
+       int n=100; //vector como entero, con dimension de 100
        int[] vector = new int[n]; 
-    //Con un for voy a ir recorriendo todas las posiciones de sus elementos   
-       for(int i = 0; i<100; i++){
-    //Para que sea en forma descendiente mi vector[i] = 100 y va restando de 1 en 1       
-           vector[i]=100-i; 
-           System.out.println("["+vector[i]+"]");
+    
+       for(int i = 0; i<100; i++){ //Con un for voy a ir recorriendo todas las posiciones de sus elementos   
+      
+           vector[i] = 100-i;    //Para que sea en forma descendiente mi vector[i] = 100 y va restando de 1 en 1   
+           System.out.println("[" + vector[i] + "]");
         }
     }   
     
