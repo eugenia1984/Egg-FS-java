a808aaeab127be510aa97b80e87c0c078ad570a4
Eugenia <costamariaeugenia1@gmail.com>
2021-08-21 16:39:55 -0300

    video POO
    con un clase Perro con sus atributos instancio dos nuevos objetos perro1 y perro2 de la clase Perro

---
+++ guia6-java-POO/VideoPOO/src/videopoo/Perro.java
@@ -0,0 +1,15 @@
+/*
+* Mi clase Perro a partir de la cual voy a instanciar un objeto de tipo Perro
+* Esta CLASE va a ser un MOLDE para instancir (crear) nuevos OBJETOS
+ */
+package videopoo;
+
+public class Perro {
+    
+    //Los ATRIBUTOS de la clase, que al ser public van a poder ser accedidos 
+    //de forma directa por otras clases
+    public String nombre;
+    public String raza;
+    public int edad;
+    
+}
---
+++ guia6-java-POO/VideoPOO/src/videopoo/VideoPOO.java
@@ -0,0 +1,15 @@
+/*
+ * Instancio a un nuevo perro a partir de mi Clase Perro
+ */
+package videopoo;
+
+public class VideoPOO {
+
+    public static void main(String[] args) {
+        // Instancio perro1 a partir de mi clase Perro
+        Perro perro1 = new Perro();
+        // Instancio perro2 a partir de mi clase Perro
+        Perro perro2 = new Perro();
+    }
+    
+}
