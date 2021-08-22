563bf1867d88edfb5a47f1bb2e96d21afe448a44
Eugenia <costamariaeugenia1@gmail.com>
2021-08-22 12:32:28 -0300

    Update Ejercicio10.java

--- guia6-java-POO/Ejercicio10/Ejercicio10.java
+++ guia6-java-POO/Ejercicio10/Ejercicio10.java
@@ -1,3 +1,14 @@
+/**  Realizar un programa en Java donde se creen dos arreglos: el primero será un 
+ * arreglo A de 50 números reales, y el segundo B, un arreglo de 20 números, 
+ * también reales.
+ * El programa deberá inicializar el arreglo A con números aleatorios y 
+ * mostrarlo por pantalla.
+ * Luego, el arreglo A se debe ordenar de menor a mayor y copiar los primeros 
+ * 10 números ordenados al arreglo B de 20 elementos, y rellenar los 10 últimos
+ * elementos con el valor 0.5.
+ * Mostrar los dos arreglos resultantes: el ordenado de 50 elementos y el 
+ * combinado de 20.
+ */
 package Ejercicio10;
 import java.util.Arrays;
 
